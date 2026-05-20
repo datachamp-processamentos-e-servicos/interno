329cd5d1cc87b0e016926131fdb3f93c19931720
Augusto Foppa <augusto@datachamp.com.br>
2026-05-20 15:45:58 -0300

    Remove build do versionamento

--- build/assets/js/36994c47.fe952854.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkteste=self.webpackChunkteste||[]).push([["191"],{5516(e){e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);
--- build/assets/js/552.d6681c27.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkteste=self.webpackChunkteste||[]).push([["552"],{1611(e,s,t){t.r(s)}}]);
